17d7feea2c03b08f331f679f797d53d77ee899f3
Xenopartyyy <willyawan.m@gmail.com>
2023-05-11 10:12:03 +0700

    [FIX] Perbaikan code pada bagian update

--- php CRUD data user tugas 19/update.php
+++ php CRUD data user tugas 19/update.php
@@ -19,7 +19,7 @@ if(isset($_POST['submit'])) {
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-    // Check if image file is a actual image or fake image
+    // Periksa apakah file gambar adalah gambar asli atau gambar palsu
     if(isset($_POST["submit"])) {
       $check = getimagesize($_FILES["avatar"]["tmp_name"]);
       if($check !== false) {
@@ -29,29 +29,29 @@ if(isset($_POST['submit'])) {
       }
     }
 
-    // Check if file already exists
+    // Periksa apakah avatar tersebut sudah ada / sama
     if (file_exists($target_file)) {
       echo "Sorry, file already exists.";
       $uploadOk = 0;
     }
 
-    // Check file size
+    // Periksa ukuran avatar
     if ($_FILES["avatar"]["size"] > 500000) {
       echo "Sorry, your file is too large.";
       $uploadOk = 0;
     }
 
-    // Allow certain file formats
+    // Membatasi extensi file yang bisa di upload
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
     && $imageFileType != "gif" ) {
       echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
       $uploadOk = 0;
     }
 
-    // Check if $uploadOk is set to 0 by an error
+    // Memeriksa jika $uploadOk diatur menjadi 0 karna eror atau bukan
     if ($uploadOk == 0) {
       echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
+    // jika kondisi sudah terpenuhi lalu mengunggah file gambar
     } else {
       if (move_uploaded_file($_FILES["avatar"]["tmp_name"], $target_file)) {
         // Update data ke dalam tabel users
